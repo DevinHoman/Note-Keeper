b8fa00e1c31a9dca10a35c100e933312798f440b
Devin <devinhoman93@gmail.com>
2019-07-28 20:29:30 +0200

    commit number = 28-07-2019
    Setting activity finish
    
    Start SQlite implementation

--- app/src/main/java/com/example/notekeeper/Main2Activity.java
+++ app/src/main/java/com/example/notekeeper/Main2Activity.java
@@ -1,12 +1,16 @@
 package com.example.notekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.preference.PreferenceManager;
 import android.view.View;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -17,6 +21,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -27,6 +32,9 @@ public class Main2Activity extends AppCompatActivity
     private LinearLayoutManager notesLinearManager;
     private CourseRecyclerAdapter courseRecyclerAdapter;
     private GridLayoutManager coursesLayoutManager;
+    private NoteKeeperOpenHelper mDbOpenHelper;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,9 @@ public class Main2Activity extends AppCompatActivity
                 startActivity(new Intent(Main2Activity.this,MainActivity.class));
             }
         });
+
+        mDbOpenHelper = new NoteKeeperOpenHelper(this);
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -57,6 +68,27 @@ public class Main2Activity extends AppCompatActivity
         super.onResume();
         // adapterNote.notifyDataSetChanged();
         noteRecyclerAdapter.notifyDataSetChanged();
+        updateNavHeader();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mDbOpenHelper.close();
+        super.onDestroy();
+    }
+
+    private void updateNavHeader() {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView textUserName = (TextView)headerView.findViewById(R.id.text_user_name);
+        TextView textEmailAdress = (TextView)headerView.findViewById(R.id.text_email_adress);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String userName = pref.getString("key_signature" ,"");
+        String emailAdress = pref.getString("key_email","");
+
+        textUserName.setText(userName);
+        textEmailAdress.setText(emailAdress);
     }
 
     private void initializeDisplayContent() {
@@ -82,6 +114,7 @@ public class Main2Activity extends AppCompatActivity
         recyclerItems.setAdapter(noteRecyclerAdapter);
         recyclerItems.setLayoutManager(notesLinearManager);
 
+        SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
         selectNavigationMenuItem(R.id.nav_notes);
 
     }
@@ -145,7 +178,8 @@ public class Main2Activity extends AppCompatActivity
         } else if (id == R.id.nav_courses) {
             displayCourses();
         }  else if (id == R.id.nav_share) {
-            handleSelection(R.string.nav_share_message);
+           // handleSelection(R.string.nav_share_message);
+            handleShare();
         } else if (id == R.id.nav_send) {
             handleSelection(R.string.nav_send_message);
         }
@@ -153,6 +187,14 @@ public class Main2Activity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void handleShare() {
+        View view = findViewById(R.id.list_items);
+        Snackbar.make(view,"Share to -" +
+                        PreferenceManager.getDefaultSharedPreferences(this)
+                                .getString("key_socialnetwork","")
+                                    ,Snackbar.LENGTH_LONG).show();
     }
 
     private void handleSelection(int message_id) {
